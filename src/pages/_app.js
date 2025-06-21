73128e7af644eafc016c3704638038153952a85b
charliemanter <127909405+charliemanter@users.noreply.github.com>
2025-06-21 09:34:03 -0400

    Update _app.js

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,27 @@
-import "@/styles/globals.css";
+// pages/_app.js
+import '@/styles/globals.css'
+import { useEffect } from 'react'
+import { useRouter } from 'next/router'
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  const router = useRouter()
+
+  useEffect(() => {
+    // ensure dataLayer exists
+    window.dataLayer = window.dataLayer || []
+
+    const handleRouteChange = (url) => {
+      window.dataLayer.push({
+        event: 'pageview',
+        page: url,
+      })
+    }
+
+    router.events.on('routeChangeComplete', handleRouteChange)
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange)
+    }
+  }, [router.events])
+
+  return <Component {...pageProps} />
 }
